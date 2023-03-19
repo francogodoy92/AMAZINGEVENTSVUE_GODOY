d342e5a4d402d6a81df1f48e6131031338c03334
francogodoy92 <franck92elrojo@gmail.com>
2023-03-19 18:40:28 -0300

    VuePrimeraParte

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,116 +2,90 @@ const { createApp } = Vue
 
 const url = "../assets/amazing.json"
 
-if(document.querySelector(".cards")){
-  document.querySelector(".cards").style.display = "none"
-}
-
 const app = createApp({
   data() {
     return {
       cards: [],
-      filteredCards: [],
-      categories: [],
+      cardsFiltradas: [],
       checked: [],
       search: "",
-      detail: {},
-      upcEventsByCategory: [],
-      pastEventsByCategory: [],
-      highestAttendance: {},
-      lowerAttendance: {},
-      largerCapacity: {}
+      categorias: [],
+      categoryUpcoming: [],
+      categoryPast: [],
+      mayorAsist: {},
+      menAsist: {},
+      capMaxima: {}
     }
     },created(){
       fetch(url)
       .then(res => res.json())
       .then(data => {
         if(document.title.includes("Upcoming Events")){
-          //Page Upcoming Events
+          //Filtro eventos para Upcoming Events
           this.cards = data.events.filter(event => event.date >= data.currentDate);
         }
         else if(document.title.includes("Past Events")){
-          //Page Past Events
+          //Filtro eventos para Past Events
           this.cards = data.events.filter(event => event.date < data.currentDate);
         }
         else{
           this.cards = data.events;
         }
-        this.filteredCards = this.cards;
-        this.categories = [...new Set(this.cards.map(e => e.category))];
-        //Page Details
-        const queryString = location.search;
-        const params = new URLSearchParams(queryString);
-        const id = params.get('id');
-        const $div = document.querySelector(".cardDetails");
-        this.detail = this.cards.find(elem => elem._id == id)
-        //Page Stats
-        //UPCOMING
-        this.upcEventsByCategory = this.cards.filter(elem=>{if(elem.estimate){return elem}})
+        this.cardsFiltradas = this.cards;
+        this.categorias = [...new Set(this.cards.map(e => e.category))];
+        //Próximos
+        this.categoryUpcoming = this.cards.filter(elem=>{if(elem.estimate){return elem}})
         .map(elem =>
           elem = {category: elem.category,
             revenue: elem.price*elem.capacity,
             attendance: elem.estimate/elem.capacity
           }
-        ).reduce((acc, elem) =>{
-          const categoryCoincidence = acc.find(e => e.category == elem.category);
+        ).reduce((array, elem) =>{
+          const categoryCoincidence = array.find(e => e.category == elem.category);
           if (categoryCoincidence){
             categoryCoincidence.revenue += elem.revenue;
             categoryCoincidence.attendance = (categoryCoincidence.attendance+elem.attendance)/2;
           } else{
-            acc.push(elem);
+            array.push(elem);
           }
-          return acc;
+          return array;
         },[]);
-        //PAST
-        this.pastEventsByCategory = this.cards.filter(elem=>{if(elem.assistance){return elem}})
+        //Pasados
+        this.categoryPast = this.cards.filter(elem=>{if(elem.assistance){return elem}})
         .map(elem =>
           elem = {category: elem.category,
             revenue: elem.price*elem.capacity,
             attendance: elem.assistance/elem.capacity
           }
-        ).reduce((acc, elem) =>{
-          const categoryCoincidence = acc.find(e => e.category == elem.category);
+        ).reduce((array, elem) =>{
+          const categoryCoincidence = array.find(e => e.category == elem.category);
           if (categoryCoincidence){
             categoryCoincidence.revenue += elem.revenue;
             categoryCoincidence.attendance = (categoryCoincidence.attendance+elem.attendance)/2;
           } else{
-            acc.push(elem);
+            array.push(elem);
           }
-          return acc;
+          return array;
         },[]);
-        //HIGHEST ATTENDANCE
+        // Máxima asistencia stats
         let maxAttendance = Math.max(...this.cards.map(elem => elem.assistance ? elem.assistance/elem.capacity : elem.estimate/elem.capacity));
-        let arrayHighestAttendance = this.cards.filter(elem => elem.assistance ? elem.assistance/elem.capacity == maxAttendance : elem.estimate/elem.capacity == maxAttendance);
-        let time1 = 1;
-        this.highestAttendance = arrayHighestAttendance[0];
-        setInterval(() => {
-          this.highestAttendance = arrayHighestAttendance[time1];
-          time1 < arrayHighestAttendance.length-1 ? time1++ : time1 = 0;
-        },5000)
-        // LOWER ATTENDANCE
+let arrayMayorAsist = this.cards.filter(elem => elem.assistance ? elem.assistance/elem.capacity == maxAttendance : elem.estimate/elem.capacity == maxAttendance);
+this.mayorAsist = arrayMayorAsist[0];
+        // Mínima asistencia stats
         let minAttendance = Math.min(...this.cards.map(elem => elem.assistance ? elem.assistance/elem.capacity : elem.estimate/elem.capacity));
-        let arrayLowertAttendance = this.cards.filter(elem => elem.assistance ? elem.assistance/elem.capacity == minAttendance : elem.estimate/elem.capacity == minAttendance);
-        let time2 = 1;
-        this.lowerAttendance = arrayLowertAttendance[0];
-        setInterval(() => {
-          this.lowerAttendance = arrayLowertAttendance[time2];
-          time2 < arrayLowertAttendance.length-1 ? time2++ : time2 = 0;
-        },5000)
-        //LARGER CAPACITY
+let arrayMenAsist = this.cards.filter(elem => elem.assistance ? elem.assistance/elem.capacity == minAttendance : elem.estimate/elem.capacity == minAttendance);
+this.menAsist = arrayMenAsist[0];
+        //Capacidad Máxima stats
+        
         let maxCapacity = Math.max(...this.cards.map(elem => elem.capacity));
         let arrayMaxCapacity = this.cards.filter(elem => elem.capacity == maxCapacity);
-        let time3 = 1;
-        this.largerCapacity = arrayMaxCapacity[0];
-        setInterval(() => {
-          this.largerCapacity = arrayMaxCapacity[time3];
-          time3 < arrayMaxCapacity.length-1 ? time3++ : time3 = 0;
-        },5000)
+        this.capMaxima = arrayMaxCapacity[0];
       })
       .catch(error => console.log(error))
       
     },computed:{
       filtro(){
-        this.filteredCards = this.cards.filter(elem =>
+        this.cardsFiltradas = this.cards.filter(elem =>
           (this.checked.includes(elem.category) || this.checked.length === 0)
           && elem.name.toLowerCase().includes(this.search.toLowerCase().trim())
         )
